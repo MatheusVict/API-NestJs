79386896832e6ac6b48ac95808dc8c59934fe171
MatheusVict <matheusvictorhenrique@gmail.com>
2022-11-07 16:55:01 -0300

    Database configs and .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'mysql',
+        host: ConfigService.get('DB_HOST', 'localhost'), // 1° chave de valor, 2° Valor default
+        port: Number(ConfigService.get('DB_PORT', 3306)),
+        username: ConfigService.get('DB_USER', 'root'),
+        password: '',
+        database: ConfigService.get('DB_DATABASE', 'nest'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
