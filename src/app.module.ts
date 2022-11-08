cd2bfa32973198fbcd439a994bcc781e3c8bf6a9
MatheusVict <matheusvictorhenrique@gmail.com>
2022-11-08 13:11:53 -0300

    Database configs and .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './app/todo/todo.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    TodoModule,
   ],
   controllers: [],
   providers: [],
