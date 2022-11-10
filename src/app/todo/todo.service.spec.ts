43cdf1ddf8d2adf9d56f151a57734888d86d7644
MatheusVict <matheusvictorhenrique@gmail.com>
2022-11-10 10:33:52 -0300

    Created tests firts configs providers tests

--- src/app/todo/todo.service.spec.ts
+++ src/app/todo/todo.service.spec.ts
@@ -1,21 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TodoEntity } from './entities/todo.entity';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   it('1 + 1', () => {
     expect(1 + 1).toEqual(2);
   });
-  /*let service: TodoService;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService, // Inejeção de repositorio padrão
+        {
+          provide: getRepositoryToken(TodoEntity), // Retorna ua classe de repositorio com os metodos
+          useValue: {
+            find: jest.fn(),
+            findOneBy: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            softDelete: jest.fn(),
+            merge: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<TodoService>(TodoService);
+    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(todoService).toBeDefined();
+  });
+
+  /*('findAll', () => {
+    it('should return a todo list Entity succefully',async () => {
+      
+    });
   });*/
 });
